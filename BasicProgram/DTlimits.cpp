7efd0f4692267ef3cce1f9c3d08e53f59c67fbe7
Sudarshan Bhamare <sudarshan.bhamare@gmail.com>
2020-05-17 15:57:55 +0530

    "some basic programs"

---
+++ BasicProgram/DTlimits.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main(){
+    cout << "\n Upper and lower limits : \n";
+    cout << "max of interger : " << INT_MAX << endl;
+    cout << "min of interger : " << INT_MIN << endl;
+    cout << "max of unsigned int  : " << UINT_MAX << endl;
+    cout << "long long max int : " << LLONG_MAX << endl;
+    cout << "long long min int : " << LLONG_MIN << endl;
+
+
+return 0;
+}
